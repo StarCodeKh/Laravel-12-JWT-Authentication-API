129626b84ab305606217f00cf8310286a5a3c721
StarCode Kh <starcodekh@email.com>
2025-09-16 19:40:39 +0700

    crearte CRUD User management

--- routes/api.php
+++ routes/api.php
@@ -2,14 +2,23 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\UserController;
 
 Route::prefix('auth')->group(function () {
     Route::post('register', [AuthController::class, 'register']);
     Route::post('login', [AuthController::class, 'login']);
 
     Route::middleware('auth:api')->group(function () {
-        Route::post('profile', [AuthController::class, 'profile']);
+        Route::get('profile', [AuthController::class, 'profile']);
         Route::post('logout', [AuthController::class, 'logout']);
         Route::post('refresh', [AuthController::class, 'refresh']);
     });
+});
+
+Route::middleware('auth:api')->group(function () {
+    Route::get('/users', [UserController::class,'index']); // List users
+    Route::post('/users', [UserController::class,'store']); // Create user
+    Route::get('/users/{id}', [UserController::class,'show']); // Show user
+    Route::put('/users/{id}', [UserController::class,'update']); // Update user
+    Route::delete('/users/{id}', [UserController::class,'destroy']); // Delete user
 });
